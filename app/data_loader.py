cc32f2587669fb00af541ff1aa7cb25946f38db2
Satyam Pandit <satyampandit396@gmail.com>
2026-03-11 18:09:28 +0530

    fix csv path for vercel

--- app/data_loader.py
+++ app/data_loader.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os
 
 def load_sales_data():
-    base_dir = os.path.dirname(os.path.dirname(__file__))
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     file_path = os.path.join(base_dir, "data", "sales.csv")
 
     df = pd.read_csv(file_path)
+
     return df
