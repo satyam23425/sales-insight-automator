2c1360f00d0ee918857f626b7156e150b6ac9557
Satyam Pandit <satyampandit396@gmail.com>
2026-03-11 18:13:54 +0530

    fix csv path

--- app/data_loader.py
+++ app/data_loader.py
@@ -3,9 +3,6 @@ import os
 
 def load_sales_data():
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
     file_path = os.path.join(base_dir, "data", "sales.csv")
-
     df = pd.read_csv(file_path)
-
     return df
